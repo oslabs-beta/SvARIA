6e185296c6bbbed4a1ffc614c50ebb06c732d8ca
Nicolette Sookar <nicolettesookar@gmail.com>
2024-05-25 13:22:13 -0400

    setup

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<!-- <h1>Welcome to your library project</h1>
+<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
+
+<h1>Our OSP</h1>
+<title>Our OSP Project</title>
+
