ee632ab0895159c1a49778ea6111e55debe3d76f
joggest95 <jilienthal@gmail.com>
2024-05-29 13:51:43 -0400

    minor patch

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -34,23 +34,25 @@
 <footer>
     <div class='footer-container'>
         <ul class='footer'>
-            <div class='svariaLogo'>Our Logo</div>
+            <ul class='footerBlock' id='logo'>
+                <li class='svariaLogo'>Our Logo</li>
+            </ul>
             <ul class='footerBlock' id='docs'>
-                <li><h5>documentation</h5></li>
+                <li><h5>Documentation</h5></li>
                 <li><h6>How-To's and ReadMe</h6></li>
                 <li><h6>Components</h6></li>
                 <li><h6>Examples</h6></li>
-            </ul>
-            <ul class='footerBlock' id='updates'>
-                <li><h5>Updates</h5></li>
-                <li><h6>Changelog</h6></li>
-                <li><h6>News</h6></li>
             </ul>
             <ul class='footerBlock' id='community'>
                 <li><h5>Community</h5></li>
                 <li><h6>find us on GitHub</h6></li>
                 <li><h6>Report an issue</h6></li>
                 <li><h6>Contact Us</h6></li>
+            </ul>
+            <ul class='footerBlock' id='updates'>
+                <li><h5>Updates</h5></li>
+                <li><h6>Changelog</h6></li>
+                <li><h6>News</h6></li>
             </ul>
         </ul>
     </div>
