5f31c3655005840c0bf326d83b3e5804076e75d5
Jewel Clarke <133067242+JewelClarkeNYC@users.noreply.github.com>
2024-05-30 22:19:24 -0400

    intitial tailwind setup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script>
+    import "../app.css";
     import Navbar from "$lib/Navbar.svelte";
     import Footer from "$lib/Footer.svelte";
 </script>
@@ -6,3 +7,15 @@
 <Navbar />
 <slot />
 <Footer />
+
+<!-- 
+    *General Page Structure*
+    header/navbar
+    hero
+    product description
+    Getting Started 
+    Tab: Components/Docs/Code
+    Github/faq
+    footer ()
+
+ -->
