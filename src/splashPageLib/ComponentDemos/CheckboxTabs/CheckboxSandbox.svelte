37f9c36c72b7e75b31302e52da45e18815b13f80
joggest95 <jilienthal@gmail.com>
2024-07-05 11:59:40 -0400

    added sandbox component to splash page library. functionality is limited right now

--- src/splashPageLib/ComponentDemos/CheckboxTabs/CheckboxSandbox.svelte
+++ src/splashPageLib/ComponentDemos/CheckboxTabs/CheckboxSandbox.svelte
@@ -1,1 +1,6 @@
 Sandbox will go here 
+<script>
+    import Sandbox from '../../Sandbox.svelte';
+</script>
+
+<Sandbox title="checkboxSandbox" source = "https://svelte.dev/repl/6f215987234e471fbe241df938789695?version=4.2.18"/>
---
+++ src/splashPageLib/Sandbox.svelte
@@ -0,0 +1,12 @@
+<script lang='ts'>
+	export let title: string = '';
+	export let source: string = '';
+</script>
+
+
+<iframe
+	title={title}
+	src={source}
+	width="100%"
+	height="600px"
+></iframe> 
