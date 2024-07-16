d059d9014c924ac7e0d45aa7df31b07933e6c2bc
Nicolette Sookar <nicolettesookar@gmail.com>
2024-07-15 22:16:38 -0400

    progress bar with docs

---
+++ src/lib/Tree.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let node;
+    export let isSelected;
+    export let selectNode;
+  </script>
+  
+  <li role="treeitem" {aria-expanded="!!node.children"} {aria-selected="isSelected(node)"}>
+    <div on:click={() => selectNode(node)}>{node.label}</div>
+    {#if node.children}
+      <ul role="group">
+        {#each node.children as child}
+          <TreeNode {node: child} {isSelected} {selectNode} />
+        {/each}
+      </ul>
+    {/if}
+  </li>
