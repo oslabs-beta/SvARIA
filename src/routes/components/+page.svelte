957ddada4e3feba4498dc8d015191a543feadcb5
EJSinclair <elizabethsinc.12@gmail.com>
2024-07-20 14:38:00 -0700

    commited from routes compoentes page

--- src/routes/components/+page.svelte
+++ src/routes/components/+page.svelte
@@ -1,27 +1,25 @@
 <script>
-    import ButtonTab from "../../splashPageLib/ComponentDemos/ButtonTabs/ButtonTab.svelte";
-    import ModalTab from "../../splashPageLib/ComponentDemos/ModalTabs/ModalTab.svelte"
+	import ButtonTab from '../../splashPageLib/ComponentDemos/ButtonTabs/ButtonTab.svelte';
+	import ModalTab from '../../splashPageLib/ComponentDemos/ModalTabs/ModalTab.svelte';
 </script>
 
-<style>
-    .lib-display {
-        align-items: center;
-        width: 50%;
-        height: 50%;
-    }
-</style>
-
 <h3>Components</h3>
-<div class='lib-display'>
-    <h3>Button Component</h3>
-    <div class='component'>
-    </div>
-        <ButtonTab/>
+<div class="lib-display">
+	<h3>Button Component</h3>
+	<div class="component"></div>
+	<ButtonTab />
 </div>
 
-<div class='lib-display'>
-    <h3>Modal Component</h3>
-    <div class='component'>
-    </div>
-        <ModalTab/>
+<div class="lib-display">
+	<h3>Modal Component</h3>
+	<div class="component"></div>
+	<ModalTab />
 </div>
+
+<style>
+	.lib-display {
+		align-items: center;
+		width: 50%;
+		height: 50%;
+	}
+</style>
