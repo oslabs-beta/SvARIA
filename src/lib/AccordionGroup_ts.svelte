3cacb1099a198c8431fb4b2a52bfb00af6f42aeb
EJSinclair <elizabethsinc.12@gmail.com>
2024-07-25 18:00:23 -0700

    post presention

--- src/lib/AccordionGroup_ts.svelte
+++ src/lib/AccordionGroup_ts.svelte
@@ -1,21 +1,21 @@
-<script lang='ts'>
-    import Accordion from "./Accordion_ts.svelte";
-    import type { AccordionObj } from '../types.ts'
-    
-    export let className: string = '';
-    export let group: AccordionObj[] = [];
-    export let ariaLabel = '';
+<script lang="ts">
+	import Accordion from './Accordion_ts.svelte';
+	import type { AccordionObj } from '../types.ts';
+
+	export let className: string = '';
+	export let group: AccordionObj[] = []; // I'm pretty sure that we dont need to export those variables because theya re defiend in this type
+	export let ariaLabel = '';
 
 	// const exampleGroup = [{name: 'This Accordion', heading: 'A Group', slot: }]
 </script>
 
-<div class={className} aria-label={ariaLabel} >
-    {#each group as curr}
-        <Accordion heading={curr.heading} >
-            <div slot='content' class="lib-display">
-                <h3>{curr.heading}</h3>
-                <svelte:component this={curr.slot} />
-            </div>
-        </Accordion>
-    {/each}
+<div class={className} aria-label={ariaLabel}>
+	{#each group as curr}
+		<Accordion heading={curr.heading}>
+			<div slot="content" class="lib-display">
+				<h3>{curr.heading}</h3>
+				<svelte:component this={curr.slot} />
+			</div>
+		</Accordion>
+	{/each}
 </div>
