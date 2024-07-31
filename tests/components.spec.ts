18745bcdc3a657981276edebff6ce7d4c079e7aa
joggest95 <jilienthal@gmail.com>
2024-07-31 14:59:25 -0400

    testing button and popover

--- tests/components.spec.ts
+++ tests/components.spec.ts
@@ -1,23 +1,57 @@
 import { test, expect } from '@playwright/experimental-ct-svelte';
-import Button from '../src/lib/Button_ts.svelte'
+import Button from '../src/lib/Button_ts.svelte';
+import Popover from '$lib/Popover.svelte';
 
-test('is rendered', async ({ mount }) => {
+test.describe('Button', () => {
+  let button;
   let clicked = true;
-  const button = await mount(Button, {
+
+  test.beforeEach( async ( {mount} ) => {
+    button = await mount(Button, {
     props: {
         content: "Click",
-        className: "bg-white hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full",
+        className: "testClass",
         ariaLabel: "clickMeButton",
         id: 'demobutton',
       },
     on: {
         click: (): void => {
-          console.log('yay');
+          clicked = false;
         },
       }
-    }
-  )
-  await expect(button).toContainText('Click');
-  await button.click();
-  expect(clicked).toBeFalsy;
-});
+    })
+  })
+
+
+  test('Renders with correct content', async () => {
+    await expect(button).toContainText('Click');
+  })
+
+  test('className connects to class prop', async () => {
+    await expect(button).toHaveClass('testClass');
+  })
+
+  test('ariaLabel conntents to aria-label prop', async () => {
+    await expect(button).toHaveAccessibleName('clickMeButton');
+  })
+
+  test('has id prop', async () => {
+    await expect(button).toHaveJSProperty('id', 'demobutton');
+  })
+
+  test('onClick function works', async () => {
+    await button.click();
+    expect(clicked).toBeFalsy();
+  })
+  
+})
+
+test.describe('Popover', () => {
+  test.beforeEach( async ({ mount }) => {
+    let popover = await mount(Popover, {
+      props: {
+        
+      }
+    })
+  })
+})
