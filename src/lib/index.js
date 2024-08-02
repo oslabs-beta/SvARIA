dd1ab341ca31b39d04e3fc7f34e04ad98f912b2f
EJSinclair <elizabethsinc.12@gmail.com>
2024-08-02 09:19:14 -0700

    cleaned up cc

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,13 +1,9 @@
 // Reexport your entry components here
-import Button from './Button_ts.svelte'
-import Accordion from './AccordionGroup_ts.svelte'
-
-export {Button, Accordion}// Reexport your entry components here
-import Navigation from './Navigation_ts.svelte';
+import Button from './Button_ts.svelte';
+import Navigation from './navigation/Navigation_ts.svelte';
 import Accordion from './Accordion_ts.svelte';
 import AccordionGroup from './AccordionGroup_ts.svelte';
-import Button from './Button_ts.svelte';
-import Form from './Form_ts.svelte';
+import Form from './input_types/Form_ts.svelte';
 import Menu from './Menu_ts.svelte';
 import Modal from './Modal.svelte';
 import PopOver from './Popover.svelte';
@@ -15,7 +11,7 @@ import ProgressBar from './ProgressBar.svelte';
 import RadioGroup from './RadioGroup_ts.svelte';
 import Switch from './Switch.svelte';
 import Tab from './Tab_ts.svelte';
-import { colorContrastCheck, ariaLabelcheck, parentColorContrastCheck } from './ARIAChecks.js';
+import { colorContrastCheck, ariaLabelcheck, parentColorContrastCheck } from './ARIAchecks.js';
 
 export {
 	Navigation,
--- src/routes/ESTestPage/+page.svelte
+++ src/routes/ESTestPage/+page.svelte
@@ -1,7 +1,25 @@
-<script> 
-    import Button from '$lib/Button_ts.svelte'
+<script>
+	import Button from '$lib/Button_ts.svelte';
+	const handleClick = () => {
+		console.log('click');
+	};
 </script>
 
-    <Button content = 'good button' className='bg-black text-white text-bold hover:bg-gray-700' id='good'/> 
-    <Button content = 'bad button ' className='bg-yellow-200 text-white text-bold hover:bg-gray-700' id='bad'/> 
-    <Button  content = 'okay button ' className='bg-blue-700 text-white text-bold' id='ok'/> 
+<Button
+	content="good button"
+	className="bg-black text-white text-bold hover:bg-gray-700"
+	id="good"
+	on:click={handleClick}
+/>
+<Button
+	content="bad button "
+	className="bg-white text-yellow-700 text-bold hover:bg-gray-700"
+	id="bad"
+	on:click={handleClick}
+/>
+<Button
+	content="okay button "
+	className="bg-blue-700 text-white text-bold"
+	id="ok"
+	on:click={handleClick}
+/>
