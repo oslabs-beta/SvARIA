05ac86dd5be2c0aaf01e4a796b086b81d4802b64
Nicolette Sookar <nicolettesookar@gmail.com>
2024-08-14 17:44:22 -0400

    test removing db

--- server/Controllers/svAriaController.js
+++ server/Controllers/svAriaController.js
@@ -1,107 +1,107 @@
-import db from '../index.js';
+// import db from '../index.js';
 
-const svAriaController = {};
+// const svAriaController = {};
 
 
-// To add a new user 
-svAriaController.addUser = (req, res, next) => {
-    console.log('adding some things')
-    const {first_Name, last_Name, email_} = req.body;
-    console.log('first name here' + first_Name)
-    const queryString = {
-        text: 'INSERT INTO users (firstName, lastName, email) VALUES ($1, $2, $3)',
-        values: [first_Name, last_Name, email_]
-    };
+// // To add a new user 
+// svAriaController.addUser = (req, res, next) => {
+//     console.log('adding some things')
+//     const {first_Name, last_Name, email_} = req.body;
+//     console.log('first name here' + first_Name)
+//     const queryString = {
+//         text: 'INSERT INTO users (firstName, lastName, email) VALUES ($1, $2, $3)',
+//         values: [first_Name, last_Name, email_]
+//     };
 
-    db.query(queryString)
-        .then (data => {
-            res.locals.message   = 'New User Created'
-            return next();   
-        })
-        .catch(error => { 
-            return console.log(`ERROR: ${error}`);
-        });
-};
+//     db.query(queryString)
+//         .then (data => {
+//             res.locals.message   = 'New User Created'
+//             return next();   
+//         })
+//         .catch(error => { 
+//             return console.log(`ERROR: ${error}`);
+//         });
+// };
 
-// if user needs to locate acct to update 
-svAriaController.getUser = (req, res, next) => {
-      const email = req.params.email;
-      const queryString = {
-        text: 'SELECT * FROM users WHERE email = $1',
-        values: [email]
-    };
+// // if user needs to locate acct to update 
+// svAriaController.getUser = (req, res, next) => {
+//       const email = req.params.email;
+//       const queryString = {
+//         text: 'SELECT * FROM users WHERE email = $1',
+//         values: [email]
+//     };
     
-    db.query(queryString)
-    .then(data => {
-        res.locals.user = data.rows[0];
-        return next();
-    })
-    .catch(error => { 
-        return console.log(`ERROR: ${error}`);
-    });
-};
+//     db.query(queryString)
+//     .then(data => {
+//         res.locals.user = data.rows[0];
+//         return next();
+//     })
+//     .catch(error => { 
+//         return console.log(`ERROR: ${error}`);
+//     });
+// };
 
-// if we need to pull users for email blast
-svAriaController.getAllUsers = (req, res, next) => {
-    const queryString = {
-        text: 'SELECT * FROM users'
-    };
-    db.query(queryString)
-    .then(data => {
-        res.locals.allUsers = data.rows;
-        return next();
-    })
-    .catch(error => { 
-        return console.log(`ERROR: ${error}`);
-    });  
-};
+// // if we need to pull users for email blast
+// svAriaController.getAllUsers = (req, res, next) => {
+//     const queryString = {
+//         text: 'SELECT * FROM users'
+//     };
+//     db.query(queryString)
+//     .then(data => {
+//         res.locals.allUsers = data.rows;
+//         return next();
+//     })
+//     .catch(error => { 
+//         return console.log(`ERROR: ${error}`);
+//     });  
+// };
 
-// If we need to update a user
-svAriaController.updateUser = (req, res, next) => {
-    console.log('req ' + JSON.stringify(req.query))
-    const oldEmail = req.params.email; 
-    let {first_Name, last_Name, email_} = req.query;
+// // If we need to update a user
+// svAriaController.updateUser = (req, res, next) => {
+//     console.log('req ' + JSON.stringify(req.query))
+//     const oldEmail = req.params.email; 
+//     let {first_Name, last_Name, email_} = req.query;
 
-    if(!first_Name){
-        first_Name = res.locals.user.firstname;
-    }
-    if(!last_Name){
-        last_Name = res.locals.user.lastname;
-    }
-    if(!email_){
-        email_ = oldEmail;
-    }
-    const queryString = {
-        text: 'UPDATE users SET firstName = $1, lastName = $2, email = $3 WHERE email = $4',  
-        values:[first_Name, last_Name, email_, oldEmail]
-    };
+//     if(!first_Name){
+//         first_Name = res.locals.user.firstname;
+//     }
+//     if(!last_Name){
+//         last_Name = res.locals.user.lastname;
+//     }
+//     if(!email_){
+//         email_ = oldEmail;
+//     }
+//     const queryString = {
+//         text: 'UPDATE users SET firstName = $1, lastName = $2, email = $3 WHERE email = $4',  
+//         values:[first_Name, last_Name, email_, oldEmail]
+//     };
     
-    db.query(queryString)
-    .then(data => {
-        res.locals.message = 'Update successful'
-        return next();
-    })
-    .catch(error => { 
-        return console.log(`ERROR: ${error}`);
-    });  
-};
+//     db.query(queryString)
+//     .then(data => {
+//         res.locals.message = 'Update successful'
+//         return next();
+//     })
+//     .catch(error => { 
+//         return console.log(`ERROR: ${error}`);
+//     });  
+// };
 
-//if we need to remove a user from db
-svAriaController.deleteUser = (req, res, next) => {
-    const email = req.params.email;
-    const queryString = {
-        text: 'DELETE FROM users WHERE email = $1',
-        values: [email]
-    };
+// //if we need to remove a user from db
+// svAriaController.deleteUser = (req, res, next) => {
+//     const email = req.params.email;
+//     const queryString = {
+//         text: 'DELETE FROM users WHERE email = $1',
+//         values: [email]
+//     };
 
-    db.query(queryString)
-    .then(data => {
-        res.locals.message = 'Deletion successful'
-        return next();
-    })
-    .catch(error => { 
-        return console.log(`ERROR: ${error}`);
-    });   
-};
+//     db.query(queryString)
+//     .then(data => {
+//         res.locals.message = 'Deletion successful'
+//         return next();
+//     })
+//     .catch(error => { 
+//         return console.log(`ERROR: ${error}`);
+//     });   
+// };
 
-export default svAriaController;
+// export default svAriaController;
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,24 +1,24 @@
 import express from 'express';
-import controller from '../Controllers/svAriaController.js';
+// import controller from '../Controllers/svAriaController.js';
 
 const router = express.Router();
 
-router.post('/',
-  controller.addUser,
-  (req, res) => res.status(200).json(res.locals.message)
-);
+// router.post('/',
+//   controller.addUser,
+//   (req, res) => res.status(200).json(res.locals.message)
+// );
 
 
-router.patch('/:email', controller.getUser, controller.updateUser, (req,res) => res.status(200).json(res.locals.message)
-);
+// router.patch('/:email', controller.getUser, controller.updateUser, (req,res) => res.status(200).json(res.locals.message)
+// );
 
-router.get('/:email', controller.getUser, (req, res) => res.status(200).json(res.locals.user)
-);
+// router.get('/:email', controller.getUser, (req, res) => res.status(200).json(res.locals.user)
+// );
 
-router.get('/', controller.getAllUsers, (req, res) => res.status(200).json(res.locals.allUsers)
-);
+// router.get('/', controller.getAllUsers, (req, res) => res.status(200).json(res.locals.allUsers)
+// );
 
-router.delete('/:email', controller.deleteUser, (req, res) => res.status(200).json(res.locals.message)
-);
+// router.delete('/:email', controller.deleteUser, (req, res) => res.status(200).json(res.locals.message)
+// );
 
 export default router;
