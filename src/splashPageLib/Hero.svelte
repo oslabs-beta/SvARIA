2540d650282e727d0989b877bf70a8f54da7c9df
joggest95 <jilienthal@gmail.com>
2025-03-13 11:52:37 -0400

    tiny adjustments

--- src/splashPageLib/Hero.svelte
+++ src/splashPageLib/Hero.svelte
@@ -9,7 +9,7 @@
     <!-- bg-[url(/A11yImage.png)] -->
     <div id='heroCont' class="bg-[url(/A11yImage.png)] bg-cover bg-center bg-no-repeat w-full h-[80vh] flex justify-center items-center" alt="A11y Accessibility Image">
         <div class='h-2/3 w-[80vw] flex bg-white opacity-90 rounded-xl justify-center items-center'>
-            <div class=''>
+            <div class='w-full h-full bg-red-400'>
                 “98% of U.S.-based webpages are not accessible to the disability community from a legal perspective…”
                 <br>
                 <br id=''>
